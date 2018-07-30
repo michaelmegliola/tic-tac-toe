1e183020ff60bbe7f96b5015ddad4f43e9cf5dae
michaelmegliola <michael.megliola@gmail.com>
2018-07-30 17:39:47 -0400

    um?

--- tictactoe.py
+++ tictactoe.py
@@ -8,7 +8,8 @@ class Board:
     def reset(self):
         self.board = np.zeros([3,3])
         self.moves = 0
-        
+        self.x_turn = True
+
     # returns the max and min of sum of each axis + each diagonal
     def max_min(self):
         col_sum = np.sum(self.board,0)
@@ -19,29 +20,19 @@ class Board:
         diag1 = np.flip(self.board,0).trace(0)
         return max(max(maxs), diag0, diag1), min(min(mins), diag0, diag1)
 
-    # returns winner (if one player had won), else None
-    def winner(self):
-        x,o = self.max_min()
-        if x == 3:
-            return 1
-        elif o == -3:
-            return -1
-        else:
-            return 0
-
-    def game_over(self, winner):
-        return winner != 0 or self.moves == 9
-
-    # record a move; return reward with respect to ai player
-    def move(self, action, player):
+    def move(self, action, player, verbose):
+        n_player = 1 if player is self.x_player else -1
         row = action // 3
         col = action % 3
         if self.board[row][col] == 0:
-            self.board[row][col] = player
+            self.board[row][col] = n_player
             self.moves += 1
         else:
-            raise ValueError("Illegal move.")
-        return self.winner()
+            raise ValueError("illegal move")
+        if verbose:
+            print(self)
+        x,o = self.max_min()
+        return x == 3 or o == -3
 
     def is_empty(self, row_col):
         return self.board[row_col // 3,row_col % 3] == 0
@@ -65,36 +56,26 @@ class Board:
 
     def play(self, verbose=False):
         self.reset()
-        winner = 0
-        while not self.game_over(winner):
-            x_state = self.state()
+        if verbose:
+            print('========= NEW GAME =======================')
+        while self.moves < 9:
+            player = self.x_player if self.x_turn else self.o_player
+            opponent = self.o_player if self.x_turn else self.x_player
+            state = self.state()
             try:
-                winner = self.move(self.x_player.move(self, x_state),1)
+                if self.move(player.move(self,state),player,verbose):
+                    player.update(self,state,1)
+                    opponent.update(self,state,-1)
+                    break
             except ValueError:
-                winner = -1
-            if verbose:
-                print(self)
-            if not self.game_over(winner):
-                try:
-                    winner = self.move(self.o_player.move(self, self.state()),-1)
-                except ValueError:
-                    winner = 1
-                if verbose:
-                    print(self)
-            self.x_player.update(self, x_state, winner)
-            self.o_player.update(self, self.state(), -winner)
-            if verbose and self.game_over(winner):
-                print("WINNER = ", self.winner())
+                player.update(self,state,-10)
+                break
+            self.x_turn = not self.x_turn
 
     def __str__(self):
         s = '------------------------------------------\n'
         s += self.board.__str__()
         s += '\n------------------------------------------\n'
-        s += 'x_player: '
-        s += self.x_player.__str__()
-        s += '\n'
-        s += 'o_player: '
-        s += self.o_player.__str__()
         return s
 
 class RandomPlayer:
@@ -112,7 +93,8 @@ class HumanPlayer:
         return int(input())
 
     def update(self, board, state, reward):
-        pass
+        if reward != 0:
+            print("Human player received a reward",reward)
 
     def __str__(self):
         return "I am a human player"
