e264cf2d94ae303ca3bbaf626abd6ce7fa37fb33
michaelmegliola <michael.megliola@gmail.com>
2018-07-31 16:25:01 -0400

    repairs

--- tictactoe.py
+++ tictactoe.py
@@ -1,10 +1,43 @@
 import numpy as np
 
+class EmptyPlayer:
+    def move(self, board, state):
+        pass
+
+    def update(self, board, state, reward):
+        pass
+
+    def __str__(self):
+        return 'I do nothing'
+
+class RandomPlayer:
+    def move(self, board, state):
+        return board.sample()
+
+    def update(self, board, state, reward):
+        pass
+
+    def __str__(self):
+        return 'I make random moves'
+
+class HumanPlayer:
+    def move(self, board, state):
+        print(board)
+        return int(input())
+
+    def update(self, board, state, reward):
+        if reward != 0:
+            print('Human player received a reward',reward)
+
+    def __str__(self):
+        return 'I am a human player'
+
 class Board:
-    def __init__(self, x_player, o_player):
+    def __init__(self, x_player = EmptyPlayer(), o_player = EmptyPlayer()):
         self.x_player = x_player
         self.o_player = o_player
-
+        self.reset()
+        
     def reset(self):
         self.board = np.zeros([3,3])
         self.moves = 0
@@ -53,11 +86,12 @@ class Board:
             if self.board[row][col] == 0:
                 return row * 3 + col
 
-    def state(self):
+    def state(self, player):
         i = 0
+        n = 1 if player is self.x_player else -1
         for row in range(3):
             for col in range(3):
-                i += (self.board[row][col] + 1) * (3 ** (row*3+col))
+                i += (n * self.board[row][col] + 1) * (3 ** (row*3+col))
         return int(i)
 
     def play(self):
@@ -65,7 +99,7 @@ class Board:
         while self.moves < 9:
             player = self.x_player if self.x_turn else self.o_player
             opponent = self.o_player if self.x_turn else self.x_player
-            state = self.state()
+            state = self.state(player)
             try:
                 if self.move(player.move(self,state),player):
                     player.update(self,state,1)
@@ -88,27 +122,9 @@ class Board:
                     s += '-'
                 s += '  '
             s += '\n\n'
-        s += '\n------------------------------------------\n'
+        s += 'x state = '
+        s += str(self.state(self.x_player))
+        s += ', o state = '
+        s += str(self.state(self.o_player))
         return s
 
-class RandomPlayer:
-    def move(self, board, state):
-        return board.sample()
-
-    def update(self, board, state, reward):
-        pass
-
-    def __str__(self):
-        return 'I make random moves'
-
-class HumanPlayer:
-    def move(self, board, state):
-        print(board)
-        return int(input())
-
-    def update(self, board, state, reward):
-        if reward != 0:
-            print("Human player received a reward",reward)
-
-    def __str__(self):
-        return "I am a human player"
